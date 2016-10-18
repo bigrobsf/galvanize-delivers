9df013b054f2d85454aeb78cff85f8810d8e6082
Rob <rjconner@mac.com>
2016-10-18 01:25:29 -0700

    subtotal function working

--- js/main.js
+++ js/main.js
@@ -12,32 +12,72 @@ function createOrderTable() {
   let orderTable = document.getElementById('order-table');
   let table = document.createElement('table');
   let header = createHeader();
-  // var subTotal = createSubTotal();
-  // var tax = createTax();
-  // var total = createTotal();
+  let subtotalRow = createSubtotalRow();
+  // let taxRow = createTaxRow();
+  // let totalRow = createTotalRow();
 
+  table.setAttribute("id", "items");
+  table.setAttribute("class", "striped");
+  
   orderTable.appendChild(table);
   table.appendChild(header);
-  // table.appendChild(subTotal);
-  // table.appendChild(tax);
+  table.appendChild(subtotalRow);
+  // table.appendChild(taxRow);
+  // table.appendChild(totalRow);
 }
 
 //==============================================================================
 // create order table header
 function createHeader() {
-  var tHead = document.createElement('thead');
-  var tRow  = document.createElement('tr');
-  var thItem = document.createElement('th');
-  var thPrice = document.createElement('th');
+  let tHead = document.createElement('thead');
+  let tRow  = document.createElement('tr');
+  let thItem = document.createElement('th');
+  let thPrice = document.createElement('th');
 
   thItem.textContent = "Item";
   tRow.appendChild(thItem);
   thPrice.textContent = "Price";
+  thPrice.setAttribute("class", "currency");
   tRow.appendChild(thPrice);
 
   tHead.appendChild(tRow);
 
   return tHead;
+}
+
+
+//==============================================================================
+// create order table row
+function createOrderRow(item, price) {
+  let tRow  = document.createElement('tr');
+  let tdItem = document.createElement('td');
+  let tdPrice = document.createElement('td');
+
+  tdItem.textContent = item;
+  tRow.appendChild(tdItem);
+  tdPrice.textContent = price;
+  tdPrice.setAttribute("class", "currency amount");
+  tRow.appendChild(tdPrice);
+
+  return tRow;
+}
+
+//==============================================================================
+// create subtotal row
+function createSubtotalRow() {
+  let tRow  = document.createElement('tr');
+  let tdLabel = document.createElement('td');
+  let tdSubtotal = document.createElement('td');
+
+  tdLabel.textContent = "Subtotal";
+  tdLabel.style.textAlign="right";
+  tRow.appendChild(tdLabel);
+  tdSubtotal.textContent = 0;
+  tdSubtotal.setAttribute("class", "currency");
+  tRow.appendChild(tdSubtotal);
+  tRow.setAttribute("id", "subtotal");
+
+  return tRow;
 }
 
 //==============================================================================
@@ -59,14 +99,41 @@ function clickCardHandler(event) {
   }
 
   event.preventDefault(); // prevents browser from getting another page
-  createOrderRow(event.target);
+  addOrderRow(event.target);
 }
 
 //==============================================================================
-// create order table row for the current card
-function createOrderRow(card) {
+// create order table row for the current card and insert it before subtotal row
+function addOrderRow(card) {
   let gParent = card.parentElement.parentElement;
   let item = gParent.getElementsByClassName("item-label");
   let price = gParent.getElementsByClassName("item-price");
-  console.log(item[0].textContent, price[0].textContent);
+
+  let targetRow = document.getElementById("subtotal");
+  let newRow = createOrderRow(item[0].textContent, price[0].textContent);
+  let parent = targetRow.parentNode;
+
+  parent.insertBefore(newRow, targetRow);
+  calculateSubtotal();
+}
+
+
+//==============================================================================
+// calculate / update subtotal
+function calculateSubtotal() {
+  let prices = document.getElementsByClassName("amount");
+  let subtotal = 0;
+
+  for (var i = 0; i < prices.length; i++) {
+    var priceString = prices[i].textContent;
+    var price = Number(priceString.replace(/[^0-9\.]+/g,""));
+
+    subtotal += price;
+    console.log(subtotal);
+  }
+
+  let trSubtotal = document.getElementById("subtotal");
+  let tdSubtotal = trSubtotal.lastChild;
+
+  tdSubtotal.textContent = "$" + subtotal;
 }
